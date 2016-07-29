9e5fbb4532eefd14069405b2379eb5616464b12e
Kevindeving <98kevchen@gmail.com>
2016-07-29 10:24:30 +0200

    Revert "geehrc: try selinux the hard way (do not merge)"
    This reverts commit ba51c76412d601ceb381fd12fee1deeea8b45593.

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_COPY_FILES += \
 	device/lge/geehrc/audio_policy.conf:system/etc/audio_policy.conf
 
 PRODUCT_COPY_FILES += \
-        device/lge/geehrc/set_baseband.sh:system/etc/set_baseband.sh
+        device/lge/geehrc/fetch-swv:system/bin/fetch-swv
 
 PRODUCT_COPY_FILES += \
 	device/lge/geehrc/mixer_paths.xml:system/etc/mixer_paths.xml
